dd2e2f9e8c5748a5df05429980b071434f6b200b
Paulo Roberto Medeiros <paulo@clipescola.com.br>
2026-04-03 14:11:49 -0300

    Adicionado banner para instalação do PWA.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
+import PwaInstallBanner from "./components/PwaInstallBanner.tsx";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <PwaInstallBanner />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
